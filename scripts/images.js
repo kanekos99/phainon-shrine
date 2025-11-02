a308116e5d97e7035205d61f52b8af98f5b4e462
kanekos99 <pidgeons99@gmail.com>
2025-11-02 17:18:13 +0800

    Add more content to page

---
+++ scripts/images.js
@@ -0,0 +1,69 @@
+const travel_photos = [
+  {
+    photo_url: "./assets/travel_photos/IMG_4103.jpg",
+    photo_text: "Plane ride",
+  },
+  {
+    photo_url: "./assets/travel_photos/IMG_4112.jpg",
+    photo_text: "Hotel in Chongqing",
+  },
+  {
+    photo_url: "./assets/travel_photos/IMG_4119.jpg",
+    photo_text: "Chongqing Jie Fang Bei",
+  },
+  {
+    photo_url: "./assets/travel_photos/IMG_4143.jpg",
+    photo_text: "Chongqing Raffles City",
+  },
+  {
+    photo_url: "./assets/travel_photos/IMG_4197.jpg",
+    photo_text: "Wu Long Karst",
+  },
+  {
+    photo_url: "./assets/travel_photos/IMG_4255.jpg",
+    photo_text: "River in Chongqing",
+  },
+  {
+    photo_url: "./assets/travel_photos/IMG_4268.jpg",
+    photo_text: "Bridge in Chongqing",
+  },
+  {
+    photo_url: "./assets/travel_photos/IMG_4355.jpg",
+    photo_text: "Phainon cardboard cutout in random Chongqing Mall",
+  },
+  {
+    photo_url: "./assets/travel_photos/IMG_4407.jpg",
+    photo_text: "Train ride",
+  },
+  {
+    photo_url: "./assets/travel_photos/IMG_4448.jpg",
+    photo_text: "Featuring Dromas (irl) (skeleton)",
+  },
+  {
+    photo_url: "./assets/travel_photos/IMG_4531.jpg",
+    photo_text: "Popland",
+  },
+  {
+    photo_url: "./assets/travel_photos/IMG_4581.jpg",
+    photo_text: "Chyrsanthemum exhibition",
+  },
+];
+
+const merch_photos = [
+  {
+    photo_url: "./assets/merch_photos/keychain.jpg",
+    photo_text: "Artist (Instagram): churii0601 (card holder), bb.ityl (keychain)",
+  },
+  {
+    photo_url: "./assets/merch_photos/chimera.jpg",
+    photo_text: "Artist (Xiao Hong Shu): 木瓜茶好喝",
+  },
+  {
+    photo_url: "./assets/merch_photos/badge.jpg",
+    photo_text: "Artist (Instagram): parfaitbird",
+  },
+  {
+    photo_url: "./assets/merch_photos/prints.jpg",
+    photo_text: "Artist (Instagram): churii0601 (photo card), parfaitbird (stickers)",
+  }
+];
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,29 @@
+const travel_photo_gallery_name = "#travel_photo_gallery";
+const travel_photo_gallery = $(travel_photo_gallery_name);
+
+const merch_photo_gallery_name = "#merch_photo_gallery";
+const merch_photo_gallery = $(merch_photo_gallery_name);
+
+const modalImg = document.getElementById("modal-image");
+const modalImgCaption = document.getElementById("modal-image-caption");
+
+const sections = ["#extras"];
+
+
+const imageCategories = [
+  {
+    categoryArray: travel_photos,
+    categoryElementId: travel_photo_gallery,
+  },
+  {
+    categoryArray: merch_photos,
+    categoryElementId: merch_photo_gallery,
+  },
+];
+
 const app = {
   init: function () {
-    console.log("Hello world :)");
+    loadImages();
   },
 };
 
@@ -13,29 +36,32 @@ document.addEventListener("keydown", (e) => {
 });
 
 function payRespects() {
-  const img = document.createElement("img");
+  //only works on shrine page
+  if (main_section.hasClass("active")) {
+    const img = document.createElement("img");
 
-  const randomNum = Math.floor(Math.random() * 5) + 1;
-  img.src = `./assets/phai${randomNum}.png`;
-  img.classList.add("falling-image");
+    const randomNum = Math.floor(Math.random() * 5) + 1;
+    img.src = `./assets/phai${randomNum}.png`;
+    img.classList.add("falling-image");
 
-  const x = Math.random() * (window.innerWidth - 100);
-  const y = Math.random() * (window.innerHeight - 100);
-  img.style.left = `${x}px`;
-  img.style.top = `${y}px`;
+    const x = Math.random() * (window.innerWidth - 100);
+    const y = Math.random() * (window.innerHeight - 100);
+    img.style.left = `${x}px`;
+    img.style.top = `${y}px`;
 
-  document.body.appendChild(img);
+    document.body.appendChild(img);
 
-  createConfetti(x + 30, y + 30);
+    createConfetti(x + 30, y + 30);
 
-  //press f text animation
-  const pressF = document.getElementById("f-key");
-  pressF.style.opacity = "1";
+    //press f text animation
+    const pressF = document.getElementById("f-key");
+    pressF.style.opacity = "1";
 
-  img.addEventListener("animationend", () => {
-    img.remove();
-    pressF.style.opacity = "0.5";
-  });
+    img.addEventListener("animationend", () => {
+      img.remove();
+      pressF.style.opacity = "0.5";
+    });
+  }
 }
 
 function createConfetti(x, y) {
@@ -79,3 +105,142 @@ musicBtn.addEventListener("click", () => {
     musicBtn.classList.add("fa-play-circle");
   }
 });
+
+/*--------------extra page-----------------*/
+
+const main_section_name = "#shrine-container";
+const main_section = $(main_section_name);
+
+const navLinkText = document.getElementById("nav-link-text");
+
+function viewMore(sectionId) {
+  const selectedSection = $(sectionId);
+
+  // hide all active sections
+  $(".active").hide().removeClass("active");
+
+  //show selected section
+  selectedSection.show();
+  selectedSection.addClass("active");
+  location.hash = sectionId;
+
+  //show updated link text
+  navLinkText.innerHTML = `
+        <i class="fa fa-long-arrow-left me-1" aria-hidden="true" id="back-btn"></i>
+        back
+    `;
+
+  //enable scroll
+  document.body.style.overflow = "auto";
+  document.body.style.backgroundAttachment = "fixed";
+}
+
+function backToMain(backBtn) {
+  if (!main_section.hasClass("active")) {
+    //extra page behaviour
+
+    //show updated innerhtml
+    navLinkText.innerHTML = `
+        <i class="fa fa-long-arrow-left me-1" aria-hidden="true" id="back-btn"></i>
+        back to home
+    `;
+
+    //remove ahref
+    backBtn.removeAttribute("href");
+
+    // hide all active sections
+    $(".active").hide().removeClass("active");
+
+    //show home section
+    main_section.show();
+    main_section.addClass("active");
+    location.hash = "";
+
+    //hid scroll
+    document.body.style.overflow = "none";
+    document.body.style.backgroundAttachment = "local";
+  } else {
+    //default behaviour
+    backBtn.setAttribute("href", "https://kanekos.neocities.org/");
+  }
+}
+
+function handleHashChange() {
+  if (sections.includes(window.location.hash)) {
+    const selectedSection = window.location.hash;
+    viewMore(selectedSection);
+  }
+}
+
+$(window).on("hashchange", handleHashChange);
+window.addEventListener("DOMContentLoaded", () => {
+  const currentHash = window.location.hash;
+  handleHashChange(currentHash);
+});
+
+/*------------------------ for image gallery -------------------------------- */
+
+function loadImages() {
+  imageCategories.forEach((category) => {
+    category.categoryArray.forEach((image) => {
+      let imageSrc = image.photo_url;
+
+      //for neocities
+      let externalUrl =
+        "https://kanekos99.github.io/gallery" + image.photo_url.substring(1);
+
+      let galleryClass = "gallery-thumbnail";
+
+      const imageThumbnailHTML = `
+      <img
+        src="${imageSrc}"
+        loading="lazy"
+        class="${galleryClass} img-fluid"
+        onclick="showImage(this)"
+        data-caption="${image.photo_text}"
+        data-bs-toggle="modal"
+        data-bs-target="#galleryModal"
+      />`;
+
+      category.categoryElementId.append(imageThumbnailHTML);
+    });
+  });
+}
+
+//Lazy Load Option 2 - no small image placeholder
+document.querySelectorAll("img.gallery-thumbnail").forEach((img) => {
+  img.style.opacity = 0; // start hidden
+  img.addEventListener("load", () => {
+    img.style.transition = "opacity 0.7s ease";
+    img.style.opacity = 1; // fade in once loaded
+  });
+});
+
+function showNextOrPrevImg(direction) {
+  const visibleImages = $("img:visible")
+    .not("#modal-image")
+    .not(".about-img")
+    .not(".nui-img")
+    .toArray();
+  const currentSrc = modalImg.src;
+  let currentIndex = visibleImages.findIndex((img) => img.src === currentSrc);
+  let nextIndex = currentIndex + direction;
+  if (direction === 1 && nextIndex >= visibleImages.length) {
+    nextIndex = 0;
+  } else if (direction === -1 && nextIndex === -1) {
+    nextIndex = visibleImages.length - 1;
+  }
+  showImage(visibleImages[nextIndex]);
+}
+
+function showImage(image) {
+  modalImg.style.display = "none";
+  modalImg.src = image.src;
+  modalImgCaption.innerHTML = `<p id="modal-image-caption">${image.getAttribute(
+    "data-caption"
+  )}</p>`;
+
+  modalImg.onload = function () {
+    modalImg.style.display = "block";
+  };
+}
